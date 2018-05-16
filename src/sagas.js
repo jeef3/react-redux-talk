bef36e2d9c0dfb742cf2f4afa71cf2ceab784934
Jeff Knaggs <mail@jeef3.com>
2018-05-16 20:16:10 +1200

    Handle list save

--- src/sagas.js
+++ src/sagas.js
@@ -13,6 +13,19 @@ export function* handleLoadDataRequested() {
   });
 }
 
+export function* handleUpdateList(action) {
+  const list = action.payload;
+
+  yield put({ type: 'LIST_UPDATED', payload: list });
+
+  try {
+    const savedList = yield call(Api.updateList, list);
+    yield put({ type: 'LIST_SAVE_SUCCEEDED', payload: savedList });
+  } catch (error) {
+    yield put({ type: 'LIST_SAVE_FAILED', error });
+  }
+}
+
 export function* handleUpdateCard(action) {
   const card = action.payload;
 
@@ -29,5 +42,6 @@ export function* handleUpdateCard(action) {
 export function* saga() {
   yield takeLatest('LOAD_DATA_REQUESTED', handleLoadDataRequested);
 
+  yield takeLatest('UPDATE_LIST', handleUpdateList);
   yield takeLatest('UPDATE_CARD', handleUpdateCard);
 }
