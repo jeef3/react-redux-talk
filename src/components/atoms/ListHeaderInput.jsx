dcf99529bb8c98d85c37892b02f71d626f58947a
Jeff Knaggs <mail@jeef3.com>
2018-05-22 16:23:11 +1200

    Fix the list header edit

--- src/components/atoms/ListHeaderInput.jsx
+++ src/components/atoms/ListHeaderInput.jsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
-import ListTitle from './ListTitle';
-
-const ListHeaderInput = ListTitle.extend`
+const ListHeaderInput = styled.input`
   box-sizing: border-box;
   width: 100%;
-  padding: 8px;
+  padding: 3px;
 
-  font: inherit;
+  font-size: 18px;
+  font-family: inherit;
+  font-weight: 600;
 
   border: 0;
 
-  background: none;
+  background: rgba(0, 0, 0, 0.1);
 `;
 ListHeaderInput.displayName = 'ListHeaderInput';
 
