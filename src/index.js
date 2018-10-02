2a69e987854dba3158e431c96f2081d04f1cba99
Jeff Knaggs <mail@jeef3.com>
2018-10-02 13:09:28 +1300

    endpoint

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 
 ReactDOM.render(React.createElement(Root), document.getElementById('root'));
+
+const endpoint = '';
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => {
+    const {
+      listeners: { source }
+    } = data.icestats;
+    // const { listeners } = source;
+
+    console.log(source, listeners);
+  });
